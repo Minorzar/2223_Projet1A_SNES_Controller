1f416f7a99bfa3983ad40b51d1f12b64d3512c5a
lmmas <louismmassin@gmail.com>
2023-05-15 14:21:58 +0200

    added separate init protocols for the plug and the controller, though they are the same for now. Added extra comments. Delays still to be defined.

--- Software/program/nrf24l01custom.c
+++ Software/program/nrf24l01custom.c
@@ -1,28 +1,34 @@
+//constants used for registers and commands
 uint8_t tx_command  = 0b10100000;
 uint8_t rx_command = 0b01100001;
 #define W_REGISTER_command 0x0
 #define CONFIG 0x0
 #define EN_AA 0x1
 #define EN_RXADDR 0x2
-#define SETUP_A 0x3
+#define SETUP_AW 0x3
+#define SETUP_RETR 0x4
 uint8_t CONFIG_command = W_REGISTER_command + CONFIG;
 uint8_t CONFIG_value = 0b00000010;
 uint8_t EN_AA_command = W_REGISTER_command + EN_AA;
 uint8_t EN_AA_value = 0b00000000;
 uint8_t EN_RXADDR_command = W_REGISTER_command + EN_RXADDR;
 uint8_t EN_RXADDR_value = 0b00000000;
+uint8_t SETUP_AW_command = W_REGISTER_command + SETUP_AW;
+uint8_t SETUP_AW_value = 0b00000000;
+uint8_t SETUP_RETR_command = W_REGISTER_command + SETUP_RETR;
+uint8_t SETUP_RETR_value = 0b00000000;
 
-//passage du mode STANDBY-1 au mode RX ou TX
+//Switching from STANDBY-1 mode to RX or TX mode
 void nrf24_RX_TX_Enable(void){
 	HAL_GPIO_WritePin(SPI_CE_Port, SPI_CE_Pin, GPIO_PIN_SET);
 }
 
-//passage du mode RX ou TX au mode STANDBY-1 
+//Switching from RX or TX mode to STANDBY-1 mode
 void nrf24_RX_TX_Disable(void){
 	HAL_GPIO_WritePin(SPI_CE_Port, SPI_CE_Pin, GPIO_PIN_RESET);
 }
 
-//Transmission de données sans fil
+//Transmitting data wirelessly
 void nrf24_Transmit(pData){
 	nrf24_RX_TX_Enable();
 	HAL_SPI_Transmit(&hspi1,&tx_command,1,0);
@@ -31,7 +37,7 @@ void nrf24_Transmit(pData){
 	nrf24_RX_TX_Disable();
 }
 
-//Réception de données sans fil
+//Receiving wireless data
 void nrf24_Receive(pData){
 	nrf24_RX_TX_Enable();
 	HAL_SPI_Transmit(&hspi1,&rx_command,1,0);
@@ -40,13 +46,27 @@ void nrf24_Receive(pData){
 	nrf24_RX_TX_Disable();
 }
 
-//Ecriture dans les registres du nrf24
+//Writing in the nrf24 register
 void nrf24_W_REGISTER(uint8_t register, uint8_t value){
 	HAL_SPI_Transmit(&hspi1, &register, 1, 0);
 	HAL_SPI_Transmit(&hspi1, &value, 1, 0);
 }
 
+//Controller Initialization
 void nrf24_Init_Controller(){
 	nrf24_W_REGISTER(EN_AA_command,EN_AA_value);
 	nrf24_W_REGISTER(CONFIG_command,CONFIG_value);
 	nrf24_W_REGISTER(EN_RXADDR_command,EN_AA_value);
+	nrf24_W_REGISTER(SETUP_AW_command,SETUP_AW_value);
+	nrf24_W_REGISTER(SETUP_RETR_command,SETUP_RETR_value);
+}
+
+//Console Initialization
+void nrf24_Init_plug(){
+	nrf24_W_REGISTER(EN_AA_command,EN_AA_value);
+	nrf24_W_REGISTER(CONFIG_command,CONFIG_value);
+	nrf24_W_REGISTER(EN_RXADDR_command,EN_AA_value);
+	nrf24_W_REGISTER(SETUP_AW_command,SETUP_AW_value);
+	nrf24_W_REGISTER(SETUP_RETR_command,SETUP_RETR_value);
+}
+
