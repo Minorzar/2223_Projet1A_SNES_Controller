4510a975d02286c253d092adf990f40b40b7359f
lmmas <louismmassin@gmail.com>
2023-05-15 16:38:47 +0200

    added CSN enabling at each command start and disabling at each end. Added new command nrf24_R_REGISTER for debug purposes

--- Software/program/nrf24l01custom.c
+++ Software/program/nrf24l01custom.c
@@ -17,35 +17,58 @@ uint8_t SETUP_AW_command = W_REGISTER_command + SETUP_AW;
 uint8_t SETUP_AW_value = 0b00000000;
 uint8_t SETUP_RETR_command = W_REGISTER_command + SETUP_RETR;
 uint8_t SETUP_RETR_value = 0b00000000;
+extern SPI_HandleTypeDef hspi1;
+
+void nrf24_CSN_Enable(void){
+	HAL_GPIO_WritePin(SPI_CSN_Port, SPI_CSN_Pin, GPIO_PIN_SET);
+}
+
+void nrf24_CSN_Disable(void){
+	HAL_GPIO_WritePin(SPI_CSN_Port, SPI_CSN_Pin, GPIO_PIN_RESET);
+}
 
 //Switching from STANDBY-1 mode to RX or TX mode
 void nrf24_RX_TX_Enable(void){
-	HAL_GPIO_WritePin(SPI_CE_Port, SPI_CE_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(SPI_CE_Port, SPI_CE_Pin, GPIO_PIN_RESET);
 }
 
 //Switching from RX or TX mode to STANDBY-1 mode
 void nrf24_RX_TX_Disable(void){
-	HAL_GPIO_WritePin(SPI_CE_Port, SPI_CE_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SPI_CE_Port, SPI_CE_Pin, GPIO_PIN_SET);
 }
 
 //Transmitting data wirelessly
 void nrf24_Transmit(uint8_t* pData){
+	nrf24_CSN_Enable();
 	HAL_SPI_Transmit(&hspi1,&tx_command,1,0);
 	HAL_SPI_Transmit(&hspi1,pData+8,1,0);
 	HAL_SPI_Transmit(&hspi1,pData,1,0);
+	nrf24_CSN_Disable();
 }
 
 //Receiving wireless data
 void nrf24_Receive(uint8_t* pData){
+	nrf24_CSN_Enable();
 	HAL_SPI_Transmit(&hspi1,&rx_command,1,0);
 	HAL_SPI_Receive(&hspi1,pData+8,1,0);
 	HAL_SPI_Receive(&hspi1,pData,1,0);
+	nrf24_CSN_Disable();
 }
 
 //Writing in the nrf24 register
 void nrf24_W_REGISTER(uint8_t reg, uint8_t value){
+	nrf24_CSN_Enable();
 	HAL_SPI_Transmit(&hspi1, &reg, 1, 0);
 	HAL_SPI_Transmit(&hspi1, &value, 1, 0);
+	nrf24_CSN_Disable();
+}
+
+//Reading the nrf24 registers
+void nrf24_R_REGISTER(uint8_t reg, uint8_t* pData){
+	nrf24_CSN_Enable();
+	HAL_SPI_Transmit(&hspi1, &reg, 1, 0);
+	HAL_SPI_Receive(&hspi1,pData,1,0);
+	nrf24_CSN_Disable();
 }
 
 //Controller Initialization
